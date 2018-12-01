7bd38f382023b0462c2d654cae938109363238cd
M. Hogeveen <mhogeveen94@gmail.com>
2018-12-01 17:14:34 +0100

    Animation between menu styles complete

--- public/resources/javascript/main.js
+++ public/resources/javascript/main.js
@@ -1,32 +1,92 @@
 $(document).ready(function() {
+
+    // Change color of navigation border on hover (normal layout)
   $('.home').hover(function() {
-    $('#navigation').css('border-right', '3px solid #112f41');
+    $('#navigation-out').css('border-right', '3px solid #112f41');
     }, function() {
-    $('#navigation').css('border-right', '3px solid #eef6ff');
+    $('#navigation-out').css('border-right', '3px solid #eef6ff');
   });
 
   $('.about-me').hover(function() {
-    $('#navigation').css('border-right', '3px solid #068587');
+    $('#navigation-out').css('border-right', '3px solid #068587');
     }, function() {
-    $('#navigation').css('border-right', '3px solid #eef6ff');
+    $('#navigation-out').css('border-right', '3px solid #eef6ff');
   });
 
   $('.skills').hover(function() {
-    $('#navigation').css('border-right', '3px solid #4fb99f');
+    $('#navigation-out').css('border-right', '3px solid #4fb99f');
     }, function() {
-    $('#navigation').css('border-right', '3px solid #eef6ff');
+    $('#navigation-out').css('border-right', '3px solid #eef6ff');
   });
 
   $('.portfolio').hover(function() {
-    $('#navigation').css('border-right', '3px solid #f2b134');
+    $('#navigation-out').css('border-right', '3px solid #f2b134');
     }, function() {
-    $('#navigation').css('border-right', '3px solid #eef6ff');
+    $('#navigation-out').css('border-right', '3px solid #eef6ff');
   });
 
   $('.contact').hover(function() {
-    $('#navigation').css('border-right', '3px solid #ed553b');
+    $('#navigation-out').css('border-right', '3px solid #ed553b');
     }, function() {
-    $('#navigation').css('border-right', '3px solid #eef6ff');
+    $('#navigation-out').css('border-right', '3px solid #eef6ff');
+  });
+
+  // Change color of navigation border on hover (button layout)
+  $('.slider-bt').hover(function() {
+    $('#navigation-in').css('border-right', '3px solid #666666');
+    }, function() {
+    $('#navigation-in').css('border-right', '3px solid #eef6ff');
+  });
+
+  $('.home-bt').hover(function() {
+    $('#navigation-in').css('border-right', '3px solid #112f41');
+    }, function() {
+    $('#navigation-in').css('border-right', '3px solid #eef6ff');
+  });
+
+  $('.about-me-bt').hover(function() {
+    $('#navigation-in').css('border-right', '3px solid #068587');
+    }, function() {
+    $('#navigation-in').css('border-right', '3px solid #eef6ff');
+  });
+
+  $('.skills-bt').hover(function() {
+    $('#navigation-in').css('border-right', '3px solid #4fb99f');
+    }, function() {
+    $('#navigation-in').css('border-right', '3px solid #eef6ff');
+  });
+
+  $('.portfolio-bt').hover(function() {
+    $('#navigation-in').css('border-right', '3px solid #f2b134');
+    }, function() {
+    $('#navigation-in').css('border-right', '3px solid #eef6ff');
+  });
+
+  $('.contact-bt').hover(function() {
+    $('#navigation-in').css('border-right', '3px solid #ed553b');
+    }, function() {
+    $('#navigation-in').css('border-right', '3px solid #eef6ff');
+  });
+
+  // Menu in/out functionality
+  // $('.slider').click(function() {
+  //   $('#navigation-out').animate('left', '-21%');
+  //   $('#navigation-in').css('left', '0');
+  // });
+  //
+  // $('.slider-bt').click(function() {
+  //   $('#navigation-in').css('left', '-8%');
+  //   $('#navigation-out').css('left', '0');
+  // });
+
+  $('.slider').click(function() {
+    $('#navigation-out').animate({'left': '-21%'});
+    $('#navigation-in').animate({'left': '0'});
+  });
+
+  $('.slider-bt').click(function() {
+    $('#navigation-in').animate({'left': '-8%'});
+    $('#navigation-out').animate({'left': '0'});
   });
 
 });
