47b3d469b8032ce9a4a863cb9d7bf3f0c8ccb772
M. Hogeveen <mhogeveen94@gmail.com>
2018-12-01 13:18:35 +0100

    Temporary home content added

--- public/resources/javascript/main.js
+++ public/resources/javascript/main.js
@@ -2,31 +2,31 @@ $(document).ready(function() {
   $('.home').hover(function() {
     $('#navigation').css('border-right', '3px solid #112f41');
     }, function() {
-    $('#navigation').css('border-right', '3px solid #000000');
+    $('#navigation').css('border-right', '3px solid #eef6ff');
   });
 
   $('.about-me').hover(function() {
     $('#navigation').css('border-right', '3px solid #068587');
     }, function() {
-    $('#navigation').css('border-right', '3px solid #000000');
+    $('#navigation').css('border-right', '3px solid #eef6ff');
   });
 
   $('.skills').hover(function() {
     $('#navigation').css('border-right', '3px solid #4fb99f');
     }, function() {
-    $('#navigation').css('border-right', '3px solid #000000');
+    $('#navigation').css('border-right', '3px solid #eef6ff');
   });
 
   $('.portfolio').hover(function() {
     $('#navigation').css('border-right', '3px solid #f2b134');
     }, function() {
-    $('#navigation').css('border-right', '3px solid #000000');
+    $('#navigation').css('border-right', '3px solid #eef6ff');
   });
 
   $('.contact').hover(function() {
     $('#navigation').css('border-right', '3px solid #ed553b');
     }, function() {
-    $('#navigation').css('border-right', '3px solid #000000');
+    $('#navigation').css('border-right', '3px solid #eef6ff');
   });
 
 });
